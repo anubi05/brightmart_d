e045c1589a77f2bf4e79fff680c6e47171612a5f
anubi05 <p46155@irma.ac.in>
2026-09-17 16:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the saved model
-model = pickle.load(open('linear.sav', 'rb'))
+model = pickle.load(open('linear', 'rb'))
 
 st.title('Sales Prediction App')
 
